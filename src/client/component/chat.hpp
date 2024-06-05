e7efa9f429d6875584ca30b8072d1ce3f3dd002b
Michael <github@supitsmike.dev>
2024-06-05 09:36:34 -0400

    Remove all components.

--- src/client/component/chat.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-namespace chat
-{
-	const char* get_client_name(const uint64_t xuid);
-}
--- src/client/component/party.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <utils/info_string.hpp>
-
-#include "game/game.hpp"
-
-namespace party
-{
-	bool is_host(const game::netadr_t& addr);
-}
